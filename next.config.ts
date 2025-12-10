260d98eb98286c88acb2bbc129040fe092582327
sduddla <sduddla921@gmail.com>
2025-12-11 00:18:57 +0900

    chore: svgr 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,25 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  turbopack: {
+    rules: {
+      '**/icons/*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
+
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      include: /[\\/]icons[\\/]/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
---
+++ src/assets/index.ts
@@ -0,0 +1,17 @@
+import Logo from '@/assets/icons/logo.svg';
+import VerticalLogo from '@/assets/icons/vertical-logo.svg';
+import VerticalLogoWhite from '@/assets/icons/vertical-logo-white.svg';
+import DefaultProfile from '@/assets/icons/default-profile.svg';
+import SymbolLogo from '@/assets/icons/symbol-logo.svg';
+import User from '@/assets/icons/user.svg';
+import Logout from '@/assets/icons/logout.svg';
+
+export {
+  Logo,
+  VerticalLogo,
+  VerticalLogoWhite,
+  DefaultProfile,
+  SymbolLogo,
+  User,
+  Logout,
+};
