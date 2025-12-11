5cde03c71fa1cd17c98bd78a90530b186679895d
sduddla <sduddla921@gmail.com>
2025-12-11 21:40:37 +0900

    chore: ESLint 경고 수정

--- src/components/common/SideBanner.tsx
+++ src/components/common/SideBanner.tsx
@@ -3,7 +3,7 @@ export default function SideBanner() {
   return (
     <div className='w-1/2 h-full bg-(--color-primary-color1) flex flex-col items-center justify-center gap-(--spacing-36)'>
       <VerticalLogoWhite width={264} height={200} />
-      <p className='text-white font-semibold text-[20px] leading-[24px]'>
+      <p className='text-white font-semibold text-[20px] leading-6'>
         개발자를 위한 타이머
       </p>
     </div>
