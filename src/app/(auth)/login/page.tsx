5182bdd0e0f3e7dab008cc852538fc0ed71dc363
sduddla <sduddla921@gmail.com>
2025-12-11 22:11:00 +0900

    style: 로그인 페이지 UI 레이아웃 적용

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,14 @@
+import SymbolLogo from '@/assets/icons/symbol-logo.svg';
+import LoginForm from '@/components/login/LoginForm';
+
 export default function LoginPage() {
-  return <div>Login Page</div>;
+  return (
+    <div className='h-screen flex items-center justify-center relative overflow-hidden'>
+      <div className='absolute top-[60px] right-0'>
+        <SymbolLogo width={872} height={530} />
+      </div>
+
+      <LoginForm />
+    </div>
+  );
 }
